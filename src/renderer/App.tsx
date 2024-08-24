4d72ad695740a5ab24f2c383627d30d8586bcc43
garvsl <garvsehgal7@gmail.com>
2024-08-24 16:36:30 -0400

    protected auth route

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,12 +1,33 @@
-import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
+/* eslint-disable react/function-component-definition */
+import {
+  MemoryRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+  Outlet,
+} from 'react-router-dom';
 import './App.css';
+import { useState } from 'react';
 import Home from './routes/Home';
+import Auth from './routes/Auth';
+
+const ProtectedRoute = ({ children, user }: any) => {
+  if (!user) {
+    return <Navigate to="/auth" replace />;
+  }
+
+  return children || <Outlet />;
+};
 
 export default function App() {
+  const [user, setUser] = useState<any>(null);
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route element={<ProtectedRoute user={user} />}>
+          <Route path="/" element={<Home />} />
+        </Route>
+        <Route path="/auth" element={<Auth setUser={setUser} />} />
       </Routes>
     </Router>
   );
