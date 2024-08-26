90a165eeb0da7576de40accbcbdc8a4f6006da35
garvsl <garvsehgal7@gmail.com>
2024-08-25 22:57:49 -0400

    session login

--- src/main/handles/login.tsx
+++ src/main/handles/login.tsx
@@ -1,4 +1,5 @@
 import bcrypt from 'bcrypt';
+import { v4 as uuidv4 } from 'uuid';
 
 const login = async (password: any, prisma: any) => {
   try {
@@ -12,6 +13,19 @@ const login = async (password: any, prisma: any) => {
     const match = await bcrypt.compare(password, user.hashedPassword);
 
     if (match) {
+      const session = await prisma.session.create({
+        data: {
+          sid: uuidv4(),
+          data: JSON.stringify({
+            userId: user.id,
+          }),
+          expiresAt: new Date('9999-12-31T23:59:59Z'),
+        },
+      });
+      sessionStorage.setItem('sessionId', session.sid);
+
+      //   now store the sessionId somewhere to get the user later
+
       return { success: true, user };
     }
     return { success: false };
